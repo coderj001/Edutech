8095a6da5f417e5595f1ff80e65a8f1e9a2896b1
mr-zero <amirajubolchi001@gmail.com>
2020-11-03 18:32:01 +0530

    added heroku files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh -c 'cd ./djangodemotask/ && exec gunicorn djangodemotask.wsgi --log-file -'
--- djangodemotask/djangodemotask/asgi.py
+++ djangodemotask/djangodemotask/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangodemotask.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'djangodemotask.settings.production')
 
 application = get_asgi_application()
