77b779f1f30ef9dea329b5321b0e60cd81d2b4b1
mr-zero <amirajubolchi001@gmail.com>
2020-11-04 12:19:36 +0530

    modified all settings

--- djangodemotask/djangodemotask/settings/development.py
+++ djangodemotask/djangodemotask/settings/development.py
@@ -30,3 +30,11 @@ INTERNAL_IPS = [
     '127.0.0.1',
     'localhost',
 ]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- djangodemotask/djangodemotask/settings/production.py
+++ djangodemotask/djangodemotask/settings/production.py
@@ -1,9 +1,16 @@
+# Production settings
+
+import os
+import dj_database_url
 from .base import *
 
 DEBUG = False
 
 # Add on production
 ALLOWED_HOSTS = ['djangodemotask.herokuapp.com', 'localhost']
+
+
+DATABASES = {'default': dj_database_url.parse(os.environ['DATABASE_URL'])}
 
 CACHES = {
     'default': {
@@ -15,15 +22,38 @@ CACHES = {
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
         },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
     },
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            'level': 'DEBUG',
+            'propagate': True,
         },
-    },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
 }
